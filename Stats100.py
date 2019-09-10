19c40fe79fc071daef63e64a63258db3f22bdbeb
Connor House <connorhouse19@gmail.com>
2019-09-09 22:01:21 -0400

    q

--- Stats100.py
+++ Stats100.py
@@ -14,8 +14,6 @@ for First100Avg in range(1,200,2):
 c = c/100
 print(c)
 
-
-
                 # Create a file called Stats100.py. Create functions to complete the following:
                 # 2.8 Compute the sum of the first 100 even numbers. Call the function First100EvenSum.
                 # 2.9 Compute the sum of the first 50 odd numbers. Call the function First50OddSum.
---
+++ boolean.py
@@ -0,0 +1,11 @@
+y = int(input("Enter a number here: "))
+if 0 < y < 10+1:
+    print("True")
+
+else:
+    print("False")
+
+                        # Create a file called boolean.py.
+                        # (2.27) Write a compound Boolean expression that returns True if the value of the variable count is between 1 and 10 inclusive.
+                        # Run the expression with a value of 1, 5, 9, 10, and 12.
+                        # Output the value of count and the return value for each.
