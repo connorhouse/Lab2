f37e69170d212e632f6418a7c1921ef01650db9d
Connor House <connorhouse19@gmail.com>
2019-09-09 21:28:19 -0400

    a

--- Stats100.py
+++ Stats100.py
@@ -1,25 +1,18 @@
 a = 0
-for first100EvenSum in range(0,201,2):
-    a = a + first100EvenSum #can use += too
+for First100EvenSum in range(0,201,2):
+    a = a + First100EvenSum
 print(a)
 
 b = 0
-for first50OddSum in range(1,100,2):
-    b = b + first50OddSum #can use += too
+for First50OddSum in range(1,100,2):
+    b = b + First50OddSum
 print(b)
 
 c = 0
-for first100Avg in range(1,200,2):
-    c = c + first100Avg #can use += too
+for First100Avg in range(1,200,2):
+    c = c + First100Avg
 c = c/100
 print(c)
-
-
-
-
-
-
-
 
 
 
